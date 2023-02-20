644d401e2c152bb626b1696e145f3dd093c3125b
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2023-02-20 15:12:27 +0200

    Chat update

--- controllers/chatsController.js
+++ controllers/chatsController.js
@@ -42,44 +42,97 @@ exports.users = async function() {
     return users;
 }
 
-exports.filter = async function(age, lookingFor, country, city) {
+exports.filter = async function(age, gender, country, city) {
     const { Op } = require('sequelize');
     const User = require('../models/User');
     const Country = require('../models/Country');
     var usersList = [];
 
-    if(country != '') {
+    let filteredUsers = [];
+    let allUsers = await User.findAll();
+    // if(gender !== '') {
+    //     for (let i = 0; i < allUsers.length; i++) {
+    //         if(allUsers[i].gender === gender) {
+    //             filteredUsers.push(allUsers[i]);
+    //         }    
+    //     }
+    // }
+    if(country !== '') {
         country = await Country.findOne({
-            where: { name: country },
-            attributes: ['code']
+            where: { name: country }
         });
-    }
-    let findUsers = await User.findAll({
-        where: {
-            [Op.or]: [
-                { gender: lookingFor ? lookingFor : null },
-                { country: country ? country.code : null },
-                { city: city ? city : null },
-            ]
-        },
-    });
-    usersList = findUsers;
-    if(age != '') {
-        if(findUsers.length > 0) {
-            usersList = calculateAge(findUsers, age);
-        } else {
-            let users = await User.findAll();
-            usersList = calculateAge(users, age);
+        for (let i = 0; i < allUsers.length; i++) {
+            if(allUsers[i].country === country) {
+                filteredUsers.push(allUsers[i]);
+            }
         }
     }
-    for (let i = 0; i < usersList.length; i++) {
-        let nm_date = (usersList[i].birthDate).replace('-', '/').replace('-', '/');
-        let date = new Date(nm_date);
-        let month_diff = Date.now() - new Date(date);
-        let age_dt = new Date(month_diff); 
-        let year = age_dt.getUTCFullYear();
-        let age = Math.abs(year - 1970);
-        usersList[i].age = age;
+    else if(city !== '') {
+        for (let i = 0; i < allUsers.length; i++) {
+            if(allUsers[i].city === city) {
+                filteredUsers.push(allUsers[i]);
+            }
+        }
     }
-    return usersList;
+    else if(gender !== '') {
+        console.log('bbbbbbbbbbbbbbbbbbbbbbbb');
+        for (let i = 0; i < allUsers.length; i++) {
+            if(allUsers[i].gender === gender) {
+                filteredUsers.push(allUsers[i]);
+            }
+        }
+    }
+    else if(age !== '') {
+        console.log(age);
+        for (let i = 0; i < allUsers.length; i++) {
+            let nm_date = (allUsers[i].birthDate).replace('-', '/').replace('-', '/');
+            let date = new Date(nm_date);
+            let month_diff = Date.now() - new Date(date);
+            let age_dt = new Date(month_diff); 
+            let year = age_dt.getUTCFullYear();
+            let age = Math.abs(year - 1970);
+            allUsers[i].age = age;
+            
+            if(allUsers[i].age == Number(age)) {
+                console.log(allUsers[i]);
+                filteredUsers.push(allUsers[i]);
+            }
+        }
+    }
+
+    // if(country != '') {
+    //     country = await Country.findOne({
+    //         where: { name: country },
+    //         attributes: ['code']
+    //     });
+    // }
+    // let findUsers = await User.findAll({
+    //     where: {
+    //         [Op.or]: [
+    //             { gender: gender ? gender : null },
+    //             { country: country ? country.code : null },
+    //             { city: city ? city : null },
+    //         ]
+    //     }
+    // });
+    // usersList = findUsers;
+    // if(age != '') {
+    //     if(findUsers.length > 0) {
+    //         usersList = calculateAge(findUsers, age);
+    //     } else {
+    //         let users = await User.findAll();
+    //         usersList = calculateAge(users, age);
+    //     }
+    // }
+    // for (let i = 0; i < usersList.length; i++) {
+    //     let nm_date = (usersList[i].birthDate).replace('-', '/').replace('-', '/');
+    //     let date = new Date(nm_date);
+    //     let month_diff = Date.now() - new Date(date);
+    //     let age_dt = new Date(month_diff); 
+    //     let year = age_dt.getUTCFullYear();
+    //     let age = Math.abs(year - 1970);
+    //     usersList[i].age = age;
+    // }
+    // console.log(filteredUsers);
+    return filteredUsers;
 }
