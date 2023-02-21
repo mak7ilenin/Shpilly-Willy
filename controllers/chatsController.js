d664a6ecb199d6bbd51f11e451e8c8796f1a5180
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2023-02-21 17:55:02 +0200

    Done with filters....

--- controllers/chatsController.js
+++ controllers/chatsController.js
@@ -1,34 +1,12 @@
-function calculateAge(f_usersList, age) {
-    let usersList = [];
-    let users = [];
-    for (let i = 0; i < f_usersList.length; i++) {
-        let user = f_usersList[i];
-        let nm_date = (user.birthDate).replace('-', '/').replace('-', '/');
-        let date = new Date(nm_date);
-        let month_diff = Date.now() - new Date(date);
-        let age_dt = new Date(month_diff); 
-        let year = age_dt.getUTCFullYear();
-        let age = Math.abs(year - 1970);
-        user.age = age;
-        users.push(user);
-    }
-    users.forEach(user => {
-        if(user.age === Number(age)) {
-            usersList.push(user);
-        }
-    });
-    return usersList;
-}
 exports.users = async function() {
     const User = require('../models/User');
     const Country = require('../models/Country');
     let users = await User.findAll();
     for (let i = 0; i < users.length; i++) {
-        let country = await Country.findOne({
-            where: { code: users[i].country },
-            attributes: ['name']
+        let userCountry = await Country.findOne({
+            where: { code: users[i].country }
         });
-        users[i].country = country.name;
+        users[i].country = userCountry.name;
 
         // Calculate user's age
         let nm_date = (users[i].birthDate).replace('-', '/').replace('-', '/');
@@ -46,93 +24,40 @@ exports.filter = async function(age, gender, country, city) {
     const { Op } = require('sequelize');
     const User = require('../models/User');
     const Country = require('../models/Country');
-    var usersList = [];
 
     let filteredUsers = [];
     let allUsers = await User.findAll();
-    // if(gender !== '') {
-    //     for (let i = 0; i < allUsers.length; i++) {
-    //         if(allUsers[i].gender === gender) {
-    //             filteredUsers.push(allUsers[i]);
-    //         }    
-    //     }
-    // }
-    if(country !== '') {
-        country = await Country.findOne({
-            where: { name: country }
+    // Change country code to country name
+    for (let i = 0; i < allUsers.length; i++) {
+        let user_country = await Country.findOne({
+            where: { code: allUsers[i].country }
         });
-        for (let i = 0; i < allUsers.length; i++) {
-            if(allUsers[i].country === country) {
-                filteredUsers.push(allUsers[i]);
-            }
-        }
-    }
-    else if(city !== '') {
-        for (let i = 0; i < allUsers.length; i++) {
-            if(allUsers[i].city === city) {
-                filteredUsers.push(allUsers[i]);
-            }
-        }
-    }
-    else if(gender !== '') {
-        console.log('bbbbbbbbbbbbbbbbbbbbbbbb');
-        for (let i = 0; i < allUsers.length; i++) {
-            if(allUsers[i].gender === gender) {
-                filteredUsers.push(allUsers[i]);
-            }
-        }
-    }
-    else if(age !== '') {
-        console.log(age);
-        for (let i = 0; i < allUsers.length; i++) {
-            let nm_date = (allUsers[i].birthDate).replace('-', '/').replace('-', '/');
-            let date = new Date(nm_date);
-            let month_diff = Date.now() - new Date(date);
-            let age_dt = new Date(month_diff); 
-            let year = age_dt.getUTCFullYear();
-            let age = Math.abs(year - 1970);
-            allUsers[i].age = age;
-            
-            if(allUsers[i].age == Number(age)) {
-                console.log(allUsers[i]);
-                filteredUsers.push(allUsers[i]);
-            }
-        }
+        allUsers[i].country = user_country.name;
     }
 
-    // if(country != '') {
-    //     country = await Country.findOne({
-    //         where: { name: country },
-    //         attributes: ['code']
-    //     });
-    // }
-    // let findUsers = await User.findAll({
-    //     where: {
-    //         [Op.or]: [
-    //             { gender: gender ? gender : null },
-    //             { country: country ? country.code : null },
-    //             { city: city ? city : null },
-    //         ]
-    //     }
-    // });
-    // usersList = findUsers;
-    // if(age != '') {
-    //     if(findUsers.length > 0) {
-    //         usersList = calculateAge(findUsers, age);
-    //     } else {
-    //         let users = await User.findAll();
-    //         usersList = calculateAge(users, age);
-    //     }
-    // }
-    // for (let i = 0; i < usersList.length; i++) {
-    //     let nm_date = (usersList[i].birthDate).replace('-', '/').replace('-', '/');
-    //     let date = new Date(nm_date);
-    //     let month_diff = Date.now() - new Date(date);
-    //     let age_dt = new Date(month_diff); 
-    //     let year = age_dt.getUTCFullYear();
-    //     let age = Math.abs(year - 1970);
-    //     usersList[i].age = age;
-    // }
-    // console.log(filteredUsers);
+    for (let i = 0; i < allUsers.length; i++) {
+        let nm_date = (allUsers[i].birthDate).replace('-', '/').replace('-', '/');
+        let date = new Date(nm_date);
+        let month_diff = Date.now() - new Date(date);
+        let age_dt = new Date(month_diff); 
+        let year = age_dt.getUTCFullYear();
+        let ft_age = Math.abs(year - 1970);
+        allUsers[i].age = ft_age;
+    }
+
+    let filter = {};
+    if(age !== '') { filter.age = age } else {}
+    if(gender !== '') { filter.gender = gender } else {}
+    if(country !== '') { filter.country = country } else {}
+    if(city !== '') { filter.city = city } else {}
+
+    filteredUsers = allUsers.filter(function(user) {
+        for (let key in filter) {
+          if (user[key] === undefined || user[key] != filter[key])
+            return false;
+        }
+        return true;
+      });
+
     return filteredUsers;
 }
--- routes/chatsRouter.js
+++ routes/chatsRouter.js
@@ -9,23 +9,15 @@ module.exports = (app, loggedHeader) => {
             res.redirect('/');
             return;
         } else {
-            const usersList = await User.findAll();
-            for (let i = 0; i < usersList.length; i++) {
-                let nm_date = (usersList[i].birthDate).replace('-', '/').replace('-', '/');
-                let date = new Date(nm_date);
-                let month_diff = Date.now() - new Date(date);
-                let age_dt = new Date(month_diff); 
-                let year = age_dt.getUTCFullYear();
-                let age = Math.abs(year - 1970);
-                usersList[i].age = age;
-            }
-            loggedHeader(req.session).then(header => {
-                // Get all users of the opposite sex
-                res.render('chats', {
-                    header: header,
-                    users: usersList,
-                    authUser: req.session,
-                    message: ''
+            users().then(users => {
+                loggedHeader(req.session).then(header => {
+                    // Get all users of the opposite sex
+                    res.render('chats', {
+                        header: header,
+                        users: users,
+                        authUser: req.session,
+                        message: ''
+                    });
                 });
             });
         }
@@ -33,26 +25,17 @@ module.exports = (app, loggedHeader) => {
 
     router.post('/', async (req, res) => {
         if(!req.body.age && !req.body.lookingFor && !req.body.country && !req.body.city) {
-            let users = await User.findAll();
-            for (let i = 0; i < users.length; i++) {
-                let nm_date = (users[i].birthDate).replace('-', '/').replace('-', '/');
-                let date = new Date(nm_date);
-                let month_diff = Date.now() - new Date(date);
-                let age_dt = new Date(month_diff); 
-                let year = age_dt.getUTCFullYear();
-                let age = Math.abs(year - 1970);
-                users[i].age = age;
-            }
-            loggedHeader(req.session).then(header => {
-                res.status(404).render('chats', {
-                    header: header,
-                    users: users,
-                    message: 'Please fill in at least one field!'
+            users().then(users => {
+                loggedHeader(req.session).then(header => {
+                    res.status(404).render('chats', {
+                        header: header,
+                        users: users,
+                        message: 'Please fill in at least one field!'
+                    });
                 });
             });
             return;
         }
-        console.log(req.body);
         loggedHeader(req.session).then(header => {
             filter(
                 req.body.age, req.body.lookingFor, 
